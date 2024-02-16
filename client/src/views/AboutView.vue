633869e057faa02db42731167e0d1ddb94bc5706
Stanleyei <81503453+stanleyei@users.noreply.github.com>
2024-02-16 16:37:10 +0800

    更新

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,6 +1,15 @@
+<script setup>
+const useFetch = async () => {
+  const response = await fetch('http://demo.api-separation.digipack.io/api/test');
+  const data = await response.json();
+  console.log(data);
+};
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button type="button" @click="useFetch">發送請求</button>
   </div>
 </template>
 
@@ -10,6 +19,7 @@
     min-height: 100vh;
     display: flex;
     align-items: center;
+    gap: 2rem;
   }
 }
 </style>
