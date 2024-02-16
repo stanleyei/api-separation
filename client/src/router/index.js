29db900f11b5b40090443935dd89ebb0c3adb525
Stanleyei <81503453+stanleyei@users.noreply.github.com>
2024-02-17 01:20:40 +0800

    更新

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,22 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Index from '@/views/Index.vue';
+import HomeView from '@/views/HomeView.vue';
+import NotFound from '@/views/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      component: Index,
+      children: [
+        {
+          path: '',
+          name: 'index',
+          component: HomeView
+        },
+        {
+          path: '/about',
+          name: 'about',
+          // route level code-splitting
+          // this generates a separate chunk (About.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import('@/views/AboutView.vue')
+        },
+      ],
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/:pathMatch(.*)',
+      component: NotFound,
+    },
   ]
 })
 
---
+++ client/src/views/NotFound.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>查無此頁</h1>
+</template>
+
+<style scoped>
+
+</style>
