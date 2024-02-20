04c37e00d8caaf3782c0428f91946b05a0f0e5ee
Stanleyei <81503453+stanleyei@users.noreply.github.com>
2024-02-20 17:21:36 +0800

    test

--- api/config/cors.php
+++ api/config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*'],
+    'paths' => ['api/*', '/login', '/sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -13,12 +13,44 @@ const useFetch = async () => {
   const data = await response.json();
   console.log(data);
 };
+
+const getCookieValue = (name) => {
+  let b = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
+  return b ? b.pop() : '';
+}
+
+const login = () => {
+  fetch('http://localhost:8000/sanctum/csrf-cookie', {
+    credentials: 'include',
+  }).then(async response => {
+    const xsrfToken = getCookieValue('XSRF-TOKEN');
+    console.log('🚀 / login / xsrfToken:', xsrfToken);
+    // const res = await fetch('http://localhost:8000/login', {
+    //   method: 'POST',
+    //   headers: {
+    //     Accept: 'application/json',
+    //     'Content-Type': 'application/json',
+    //     'X-Requested-With': 'XMLHttpRequest',
+    //     'X-XSRF-TOKEN': response.headers.get('XSRF-TOKEN'),
+    //   },
+    //   body: JSON.stringify({
+    //     email: 'admin@gmail.com',
+    //     password: '12345678',
+    //   }),
+    //   credentials: 'include',
+    // });
+
+    // const data = await res.json();
+    // console.log(data);
+  });
+};
 </script>
 
 <template>
   <div class="about">
     <h1>This is an about page</h1>
     <button type="button" @click="useFetch">獲取資料</button>
+    <button type="button" @click="login">登入</button>
   </div>
 </template>
 
