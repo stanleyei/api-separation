8b379e56f67cda43ea2ec9a92ca56890aae4d770
Stanleyei <81503453+stanleyei@users.noreply.github.com>
2024-02-20 21:52:15 +0800

    更新

---
+++ client/src/composables/useRequest.js
@@ -0,0 +1,29 @@
+const getCookie = (name) => {
+  const cookies = document.cookie.split(';');
+  for (const cookie of cookies) {
+    const [key, value] = cookie.split('=');
+    if (key.trim() === name) return value;
+  }
+  return '';
+};
+
+const useFetch = async (url, method, body) => {
+  const response = await fetch(url, {
+    method,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'X-Requested-With': 'XMLHttpRequest',
+      'X-XSRF-TOKEN': decodeURIComponent(getCookie('XSRF-TOKEN')),
+    },
+    body: JSON.stringify(body),
+    credentials: 'include',
+  });
+
+  const data = await response.json();
+  return data;
+};
+
+export {
+  useFetch,
+};
--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,8 +1,33 @@
 <script setup>
-import { inject } from 'vue'
+import { inject } from 'vue';
+import { useFetch } from '@/composables/useRequest';
+// import axios from 'axios';
+
+// const client = axios.create({
+//   baseURL: 'http://localhost:8000',
+//   headers: {
+//     'X-Requested-With': 'XMLHttpRequest',
+//   },
+//   withCredentials: true,
+// });
+
+// client.interceptors.request.use((config) => {
+//   config.headers['X-XSRF-TOKEN'] = decodeURIComponent(getCookie('XSRF-TOKEN'));
+//   return config;
+// })
 
 const apiHost = inject('apiHost');
-const useFetch = async () => {
+
+const getCookie = (name) => {
+  const cookies = document.cookie.split(';');
+  for (const cookie of cookies) {
+    const [key, value] = cookie.split('=');
+    if (key.trim() === name) return value;
+  }
+  return '';
+};
+
+const getData = async () => {
   const response = await fetch(`${apiHost}/test`, {
     headers: {
       Accept: 'application/json',
@@ -14,42 +39,27 @@ const useFetch = async () => {
   console.log(data);
 };
 
-const getCookieValue = (name) => {
-  let b = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
-  return b ? b.pop() : '';
-}
-
-const login = () => {
-  fetch('http://localhost:8000/sanctum/csrf-cookie', {
+const login = async () => {
+  await fetch('http://localhost:8000/sanctum/csrf-cookie', {
+    headers: {
+      'X-Requested-With': 'XMLHttpRequest',
+    },
     credentials: 'include',
-  }).then(async response => {
-    const xsrfToken = getCookieValue('XSRF-TOKEN');
-    console.log('🚀 / login / xsrfToken:', xsrfToken);
-    // const res = await fetch('http://localhost:8000/login', {
-    //   method: 'POST',
-    //   headers: {
-    //     Accept: 'application/json',
-    //     'Content-Type': 'application/json',
-    //     'X-Requested-With': 'XMLHttpRequest',
-    //     'X-XSRF-TOKEN': response.headers.get('XSRF-TOKEN'),
-    //   },
-    //   body: JSON.stringify({
-    //     email: 'admin@gmail.com',
-    //     password: '12345678',
-    //   }),
-    //   credentials: 'include',
-    // });
-
-    // const data = await res.json();
-    // console.log(data);
   });
+
+  const res = useFetch('http://localhost:8000/login', 'POST', {
+    email: 'admin@gmail.com',
+    password: '12345678',
+  });
+
+  console.log(res);
 };
 </script>
 
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <button type="button" @click="useFetch">獲取資料</button>
+    <button type="button" @click="getData">獲取資料</button>
     <button type="button" @click="login">登入</button>
   </div>
 </template>
