9d86ed6ccc183da40c8262bef2c8deac9f58fb67
Stanleyei <81503453+stanleyei@users.noreply.github.com>
2025-08-25 15:31:47 +0800

    更新

--- client/src/composables/useRequest.js
+++ client/src/composables/useRequest.js
@@ -11,7 +11,7 @@ const getCookie = (name) => {
 };
 
 const useFetch = async (path, method, body) => {
-  const url = `${apiHost}/laravel${path}`;
+  const url = `${apiHost}${path}`;
   const option = {
     method,
     headers: {
